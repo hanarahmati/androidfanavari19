0235171d94dbdade7e4239a3127753874f58b492
Hana <h.rahmatzehi@behsacorp.com>
2022-07-20 20:36:55 +0430

    analytics huawei

--- app/src/main/java/co/fanavari/myapplication/huawei/HuaweiPushActivity.kt
+++ app/src/main/java/co/fanavari/myapplication/huawei/HuaweiPushActivity.kt
@@ -6,25 +6,35 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import co.fanavari.myapplication.NavigationComponentExampleActivity
 import co.fanavari.myapplication.R
 import co.fanavari.myapplication.databinding.ActivityHuaweiPushBinding
+import com.huawei.agconnect.common.network.AccessNetworkManager
 import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.analytics.HiAnalytics
+import com.huawei.hms.analytics.HiAnalyticsInstance
+import com.huawei.hms.analytics.HiAnalyticsTools
+import com.huawei.hms.analytics.type.HAEventType.SUBMITSCORE
+import com.huawei.hms.analytics.type.HAParamType.SCORE
 import com.huawei.hms.common.ApiException
 import com.huawei.hms.push.HmsMessaging
 import com.huawei.hms.push.RemoteMessage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class HuaweiPushActivity : AppCompatActivity() {
 
     private lateinit var binding: co.fanavari.myapplication.databinding.ActivityHuaweiPushBinding
+    private lateinit var instance: HiAnalyticsInstance
     override fun onCreate(savedInstanceState: Bundle?) {
 
         binding = ActivityHuaweiPushBinding.inflate(layoutInflater)
@@ -35,6 +45,9 @@ class HuaweiPushActivity : AppCompatActivity() {
             btnGetToken.setOnClickListener {
                 getToken()
                 addTopic()
+                reportAnswerEvt("test")
+                postScore(25L)
+                setUserprofile("hana")
             }
             buttonSetToken.setOnClickListener {
                 CoroutineScope(Dispatchers.IO).launch {
@@ -47,6 +60,14 @@ class HuaweiPushActivity : AppCompatActivity() {
 
             }
         }
+
+        AccessNetworkManager.getInstance().setAccessNetwork(true)
+
+        // Enable SDK log recording.
+        HiAnalyticsTools.enableLog()
+        instance = HiAnalytics.getInstance(this)
+        // Or initialize Analytics Kit with the given context.
+        instance.setUserProfile("userKey", "value")
 
     }
 
@@ -170,6 +191,39 @@ class HuaweiPushActivity : AppCompatActivity() {
         notificationManager.notify(0, builder.build())
 
 
+    }
+
+    private fun reportAnswerEvt(answer: String) {
+        // TODO: Report a custom event.
+        // Event name: Answer
+        // Event parameters:
+        //  -- question: String
+        //  -- answer: String
+        //  -- answerTime: String
+
+        // Initialize parameters.
+        val bundle = Bundle()
+        bundle.putString("question", binding.btnGetToken.getText().toString().trim())
+        bundle.putString("answer", answer)
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+        bundle.putString("answerTime", sdf.format(Date()))
+
+        // Report a custom event.
+        instance.onEvent("Answer", bundle)
+    }
+
+    private fun postScore(score: Long) {
+        // TODO: Report a score through the SUBMITSCORE event.
+        // Initialize parameters.
+        val bundle = Bundle()
+        bundle.putLong(SCORE, score)
+
+        // Report a predefined event.
+        instance.onEvent(SUBMITSCORE, bundle)
+    }
+
+    private fun setUserprofile(editFavorSport: String){
+        instance.setUserProfile("favor_sport",editFavorSport);
     }
 
     companion object {
